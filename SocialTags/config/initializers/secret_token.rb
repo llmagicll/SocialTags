f5f2c3aca2bb37666b522dcd602afd83b55312d0
magic <magic@fourdesire.com>
2012-08-20 17:07:23 +0800

    Create rails project

---
+++ SocialTags/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialTags::Application.config.secret_token = '984130b13d592a056f65256917f90724992d340cb422e60270757e593ae9be859da9905b52dfd37e00a37428790d3b4fdae9d8e95be7d70dac20faa5d2aa8faf'
